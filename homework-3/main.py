f9778996496fa4ca07b4b261a013f1b284152c92
Doczadrot <155732866+Doczadrot@users.noreply.github.com>
2024-04-17 17:07:19 +0300

    Добавлены магические методы __repr__ и __str__ в класс Item

--- homework-3/main.py
+++ homework-3/main.py
@@ -1,9 +1,7 @@
 from src.item import Item
 
 if __name__ == '__main__':
-    # Создаем экземпляр объекта Item
     item1 = Item("Смартфон", 10000, 20)
-
-    # Проверяем его представление с помощью утверждений
-    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+    print(repr(item1))  # Добавляем эту строку для вывода результата repr(item1)
+    assert repr(item1) == f"Item('Смартфон', {10000:.1f}, {20})"
     assert str(item1) == 'Смартфон'
