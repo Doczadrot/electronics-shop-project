958b641e79e5fcd594879b6345dc04a47cc81df4
Doczadrot <155732866+Doczadrot@users.noreply.github.com>
2024-04-23 16:54:49 +0300

    дополнение

--- src/phone.py
+++ src/phone.py
@@ -1,11 +1,18 @@
-# src/phone.py
 from src.item import Item
 
 class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        self.validate_number_of_sim(number_of_sim)
         self.number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        self.validate_number_of_sim(value)
+        self._number_of_sim = value
 
     def validate_number_of_sim(self, number_of_sim):
         if not isinstance(number_of_sim, int) or number_of_sim <= 0:
@@ -18,4 +25,4 @@ class Phone(Item):
             raise TypeError("Можно складывать только экземпляры классов Item и Phone.")
 
     def __repr__(self):
-        return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+        return f"Phone('{self.name}', {self.price}, {self.quantity}, {self._number_of_sim})"
